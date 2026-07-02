4db0184328f5bcdc45fdece025454eff1f89e891
kunal <kunal@kunals-MacBook-Pro.local>
2026-07-02 20:49:26 +0530

    feat(security): add reusable security module

---
+++ modules/modules/security/main.tf
@@ -0,0 +1,47 @@
+resource "aws_security_group" "alb" {
+
+  name        = "alb-sg"
+  description = "Application Load Balancer Security Group"
+
+  vpc_id = var.vpc_id
+
+  ingress {
+    description = "HTTP"
+
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+  }
+
+  ingress {
+    description = "HTTPS"
+
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+  }
+
+  egress {
+
+    from_port = 0
+    to_port   = 0
+
+    protocol = "-1"
+
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+  }
+
+  tags = {
+    Name = "alb-security-group"
+  }
+}
