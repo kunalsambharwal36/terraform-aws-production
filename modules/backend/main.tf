35277ef5cb7a43ee4797ffb15b5c1624a708d2ae
kunal <kunal@kunals-MacBook-Pro.local>
2026-07-05 10:37:41 +0530

    new

--- modules/backend/main.tf
+++ modules/backend/main.tf
@@ -3,7 +3,7 @@ resource "aws_s3_bucket" "terraform_state" {
   bucket = "cloudshop-${var.environment}-terraform-state"
 
   tags = {
-    Name = "${var.project_name}-${var.environment}-terraform-state"
+    Name = aws_s3_bucket.terraform_state.bucket
   }
 }
 
