ab7ed141c0b8199083c61bb3b6050764f552c4ad
kunal <kunal@kunals-MacBook-Pro.local>
2026-07-05 06:11:01 +0530

    added cloudwatch agent config

--- modules/compute/userdata.sh
+++ modules/compute/userdata.sh
@@ -5,7 +5,15 @@ dnf update -y
 dnf install -y docker
 
 dnf install -y amazon-cloudwatch-agent
-
+mkdir -p /opt/aws/amazon-cloudwatch-agent/etc
+cat <<EOF >/opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
+${cw_agent_config}
+EOF
+/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
+-a fetch-config \
+-m ec2 \
+-c file:/opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json \
+-s
 systemctl enable docker
 systemctl start docker
 
